c90c5e709aff286c877d1a5e498c0e67032273dc
AlexMoretz <a.moretz42@gmail.com>
2018-10-23 20:38:43 -0700

    Added Hellow World message functionality

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,4 +1,11 @@
 package HelloWorld;
 
 public class HelloWorld {
+    private static void helloWorld() {
+        System.out.println("Hello World");
+    }
+    private static void HelloWorld(String[] args)
+    {
+        helloWorld();
+    }
 }
